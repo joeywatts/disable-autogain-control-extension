1c2a7d95db9e4880a689bfd4fdf617dd29ca8c75
Joey Watts <joey.watts.96@gmail.com>
2020-11-26 00:35:54 -0500

    Add support for dynamically enabling more sites
    This commit utilizes a background script to dynamically control the
    enablement of the auto gain toggle on a per-origin basis.

--- installDisableAutogain.js
+++ installDisableAutogain.js
@@ -1,4 +1,7 @@
 (function () {
+    if (window.DISABLE_AUTOGAIN_INSTALLED) {
+        return;
+    }
     var s = document.createElement('script');
     s.src = chrome.runtime.getURL('disableAutogain.js');
     s.onload = function() {
@@ -7,3 +10,4 @@
     (document.head || document.documentElement).prepend(s);
     console.log("Installed <script> tag for disabling automatic gain control");
 })();
+var DISABLE_AUTOGAIN_INSTALLED = true;
